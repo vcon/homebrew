e20bc41bc515ad1af3a9d904d5448d5e2d2167e0
Robert Shaw <robert914@me.com>
2011-09-01 13:35:28 -0700

    Updated: atomicparsley to fd7a76c53eb8

--- Library/Formula/atomicparsley.rb
+++ Library/Formula/atomicparsley.rb
@@ -1,9 +1,12 @@
 require 'formula'
 
 class Atomicparsley < Formula
-  url 'https://bitbucket.org/wez/atomicparsley/get/0.9.4.tar.bz2'
+  # url 'https://bitbucket.org/wez/atomicparsley/get/0.9.4.tar.bz2'
   homepage 'http://bitbucket.org/wez/atomicparsley/overview/'
-  md5 'f83aa99f5476e96259257a3ec114c942'
+  # md5 'f83aa99f5476e96259257a3ec114c942'
+  url 'https://bitbucket.org/wez/atomicparsley/get/fd7a76c53eb8.tar.bz2'
+  md5 '4c271090aa5a36596696a5d460f2c6fe'
+  version '2011.03.23'
 
   def install
     system "./autogen.sh"
