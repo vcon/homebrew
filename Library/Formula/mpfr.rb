f45eb9ce69ac07b868077d0042ebd4e9822142a6
Adios <adiosf6f+github@gmail.com>
2012-01-15 05:45:38 +0800

    mpfr: make it compilable under PowerPC
    *. remove unnecessary Intel CPU checks.

--- Library/Formula/mpfr.rb
+++ Library/Formula/mpfr.rb
@@ -7,26 +7,12 @@ class Mpfr < Formula
 
   depends_on 'gmp'
 
-  def options
-    [["--32-bit", "Build 32-bit only."]]
-  end
-
   def patches
     { :p1 => "http://www.mpfr.org/mpfr-3.1.0/allpatches" }
   end
 
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
-
-    # Build 32-bit where appropriate, and help configure find 64-bit CPUs
-    # Note: This logic should match what the GMP formula does.
-    if MacOS.prefer_64_bit? and not ARGV.build_32_bit?
-      ENV.m64
-      args << "--build=x86_64-apple-darwin"
-    else
-      ENV.m32
-      args << "--build=none-apple-darwin"
-    end
 
     system "./configure", *args
     system "make install"
