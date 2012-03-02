49292bea5abc55159a4c64d1ae051101a3b1c079
Robert Shaw <robert914@me.com>
2012-03-01 20:06:03 -0800

    Updated: handbrakecli to v0.9.6

--- Library/Formula/handbrakecli.rb
+++ Library/Formula/handbrakecli.rb
@@ -2,9 +2,10 @@ require 'formula'
 
 class Handbrakecli <Formula
    # @@dmgversion = 'svn3606'
-   @@dmgversion = '0.9.5'
+   @@dmgversion = '0.9.6'
+   @@osxversion = '6'
    @@arch = 'x86_64'
-   @@dmgname = "HandBrake-#{@@dmgversion}-MacOSX.5_CLI_#{@@arch}"
+   @@dmgname = "HandBrake-#{@@dmgversion}-MacOSX.#{@@osxversion}_CLI_#{@@arch}"
    if /svn/.match(@@dmgversion)
       @@dmgurl = 'https://build.handbrake.fr/job/Mac/lastSuccessfulBuild/artifact/trunk/packages/'+@@dmgname+'.dmg'
    else
@@ -14,8 +15,7 @@ class Handbrakecli <Formula
    homepage 'http://handbrake.fr/'
    url @@dmgurl, :using => NoUnzipCurlDownloadStrategy
    version "#{/svn/.match(@@dmgversion) ? @@dmgversion.sub(/svn/,'') : @@dmgversion}"
-   # md5 '52b29610b0a6a322ca7caffc25844e2b'
-   md5 'ebb61a2eb123e49c1f6cf25f81b82a0b'
+   md5 'bfec57baa4a9e6f064cd00853964852e'
 
    # don't strip binaries
    skip_clean ['bin']
